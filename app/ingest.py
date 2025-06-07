03b082c3a297a39155107f3fd84e54f3b93ae2e0
Shafah0717 <shafahmuhammed@gmail.com>
2025-06-07 22:57:04 +0530

    first commit

---
+++ app/ingest.py
@@ -0,0 +1,13 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.vectorstores import FAISS
+
+def ingest_pdf(pdf_path,index_path="vectorstore/"):
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size =500, chunk_overlap=50)
+    chunks = splitter.split_documents(documents)
+    embeddings = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-V2")
+    vectorstore = FAISS.from_documents(chunks,embeddings)
+    vectorstore.save_local(index_path)
