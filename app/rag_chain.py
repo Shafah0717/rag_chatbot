da5cf52775e880625928f80c594a9783939716c7
Shafah0717 <shafahmuhammed@gmail.com>
2025-06-07 23:53:21 +0530

    rag_chain.py

---
+++ app/rag_chain.py
@@ -0,0 +1,19 @@
+from app.model import load_model
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
+def load_vectors(index_path="vectorstore/"):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    return FAISS.load_local(index_path,embeddings)
+
+def answer_query(query , vectorstore, model_pipeline):
+    docs = vectorstore.similarity_search(query,k=3)
+    context = "\n".join([docs.page_count for doc in docs])
+    prompt = f"Answer the question using the context below:\n\nContext:\n{context}\n\nQuestion:\n{query}\n\nAnswer:"
+ 
+    output = model_pipeline(prompt)[0]['generated_text']
+
+  
+    answer = output[len(prompt):]
+
+    return answer.strip()
